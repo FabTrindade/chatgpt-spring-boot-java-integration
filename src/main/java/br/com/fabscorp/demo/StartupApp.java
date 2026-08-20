c061a3ad1b34c226fc57da8108d0f3de6ad379ce
Fabricio Trindade <fabrtrindade@gmail.com>
2026-08-20 19:08:49 -0300

    add project

---
+++ src/main/java/br/com/fabscorp/demo/StartupApp.java
@@ -0,0 +1,13 @@
+package br.com.fabscorp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartupApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartupApp.class, args);
+	}
+
+}
