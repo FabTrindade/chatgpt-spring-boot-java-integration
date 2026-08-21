58ab1ada7032107141ac477fd83a239ef44ce59a
Fabricio Trindade <fabrtrindade@gmail.com>
2026-08-21 17:42:05 -0300

    add RestTemplate bean.

--- src/main/java/br/com/fabscorp/demo/config/OpenAiConfig.java
+++ src/main/java/br/com/fabscorp/demo/config/OpenAiConfig.java
@@ -1,11 +1,27 @@
 package br.com.fabscorp.demo.config;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
 import java.util.logging.Logger;
 
 @Configuration
 public class OpenAiConfig {
     private Logger logger = Logger.getLogger(OpenAiConfig.class.getName());
+
     @Value("${openai.api.key}")
     String openAiApiKey;
+
+    @Bean
+    RestTemplate template() {
+        logger.info("Initializing RestTemplate");
+
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getInterceptors().add((request, body, execution) -> {
+            request.getHeaders().add("Authorization", "Bearer " + openAiApiKey);
+            return execution.execute(request, body);
+        });
+        return restTemplate;
+    }
 }
